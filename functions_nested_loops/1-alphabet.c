31c18f9cb4f1bf2a653f58fab181ba84f1efb8b7
Rdrg974 <hassany.rodrigue@gmail.com>
2023-10-16 06:53:50 -0700

    Write a function that prints the alphabet, in lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabet
+ *
+ * My function
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 0; a < 26; a++)
+		_putchar(97 + a);
+	_putchar('\n');
+	return;
+}
