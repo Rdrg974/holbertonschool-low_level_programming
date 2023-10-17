619197d59cab53d46550001df92089ae9eb0b109
Rdrg974 <hassany.rodrigue@gmail.com>
2023-10-16 23:59:10 -0700

    Function that prints the sign of a number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -21,11 +21,11 @@ int print_sign(int n)
 	else if (n < 0)
 	{
 		_putchar('-');
-                return (-1);
+		return (-1);
 	}
 	else
 	{
 		_putchar('0');
-                return (0);
+		return (0);
 	}
 }
