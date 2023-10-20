8ca614e763aa8a894f9e9bc8ab6dac6f3eebf298
Rdrg974 <hassany.rodrigue@gmail.com>
2023-10-20 03:00:55 -0700

    Functions included in the file called main.h

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -9,3 +12,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void prime_factor(long int n);
+void print_number(int n);
+
+#endif
