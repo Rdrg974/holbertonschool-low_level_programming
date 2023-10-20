ebe24e80a177a47394c5a73157017ce2379bdcdd
Rdrg974 <hassany.rodrigue@gmail.com>
2023-10-20 02:14:47 -0700

    Function to print the numbers primes

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * prime_factor - finds and prints the largest prime factor
+ *
+ * @n: int
+ *
+ * My function
+ *
+ * Return: Always 0 (Success)
+ */
+
+void prime_factor(int n)
+{
+	int i = 2;
+	int vrai = 0;
+
+	while (vrai == 0)
+	{
+		if ((n % i) == 0)
+		{
+			printf("%d, ", i);
+			n = (n / i);
+		}
+		else if (n == 1)
+			vrai = 1;
+		else
+			i++;
+	}
+}
