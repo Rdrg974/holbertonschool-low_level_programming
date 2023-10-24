88103c6bb96e9ea92732159700f19a0d1d33930b
Rdrg974 <hassany.rodrigue@gmail.com>
2023-10-24 06:53:55 -0700

    Functions : Pointers, Arrays and Strings

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,5 +13,12 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 
 #endif
