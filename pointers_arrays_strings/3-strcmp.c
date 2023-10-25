2abc900ee5799822574b9270af532417503cac4c
Rdrg974 <hassany.rodrigue@gmail.com>
2023-10-25 00:41:18 -0700

    Function that compares two strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = strlen(s1);
-	int j = strlen(s2);
+	int i = 0;
 
-	if (i > j)
-		return (15);
-	else if (i < j)
-		return (-15);
-	else
-		return (0);
+	while ((s1[i] != '\0') || (s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return (s2[i] - s2[i]);
+	}
+	return (0);
 }
 
