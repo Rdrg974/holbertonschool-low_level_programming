ddef4bfe0496054f92a4601cde7b763092ca508d
Rdrg974 <hassany.rodrigue@gmail.com>
2023-10-25 07:43:44 -0700

    Function that encodes a string into 1337

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>	
+#include <string.h>
 
 /**
  * cap_string - capitalizes all words of a string
@@ -11,27 +11,30 @@
 
 char *cap_string(char *s)
 {
-	int i  = 0;
+        int i  = 0;
 
-	if ((s[i] > 96) & (s[i] < 124))
-	{
-		s[i] = s[i] - 32;
-		i++;
-	}
-	while (s[i] != '\0')
-	{
-		if (((s[i] == ' ') || (s[i] == '.') || (s[i] == '\n') || (s[i] == '\t')) & ((s[i + 1] > 96) & (s[i + 1] < 124)))
-		{
-			s[i + 1] = s[i + 1] - 32;
-			i++;
-		}
-		else if (((s[i] == '.') || (s[i] == '!') || (s[i] == '?')) & (s[i + 1] == ' ') & ((s[i + 2] > 96) & (s[i + 2] < 124)))
-		{
-			s[i + 2] = s[i + 2] - 32;
-			i++;
-		}
-		else
-			i++;
-	}
-	return (s);
+        if ((s[i] > 96) & (s[i] < 124))
+        {
+                s[i] = s[i] - 32;
+                i++;
+        }
+        while (s[i] != '\0')
+        {
+                if (((s[i] == ' ') || (s[i] == '.') || (s[i] == '\n') || (s[i] == '\t')) &
+                ((s[i + 1] > 96) & (s[i + 1] < 124)))
+                {
+                        s[i + 1] = s[i + 1] - 32;
+                        i++;
+                }
+                else if (((s[i] == '.') || (s[i] == '!') ||(s[i] == '?')) &
+                (s[i + 1] == ' ') &
+                ((s[i + 2] > 96) & (s[i + 2] < 124)))
+                {
+                        s[i + 2] = s[i + 2] - 32;
+                        i++;
+                }
+                else
+                        i++;
+        }
+        return (s);
 }
