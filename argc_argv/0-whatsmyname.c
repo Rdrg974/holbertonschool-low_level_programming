59ae121351c331ad28309d6e12550cd4cd70ed37
Rdrg974 <hassany.rodrigue@gmail.com>
2023-10-31 01:25:28 -0700

    Program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print its name
+ *
+ * @argc: an integer
+ * @argv: pointer to a char
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
