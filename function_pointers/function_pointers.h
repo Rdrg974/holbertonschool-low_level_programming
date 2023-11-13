c16c8d9b713f2b3de9212f10a83a3a1537310d9f
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-13 05:31:37 -0800

    Functions pointers

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef __FUNCTION__POINTERS__H__
+#define __FUNCTION__POINTERS__H__
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
