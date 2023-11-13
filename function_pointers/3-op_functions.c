6531d2070790858a8101d01910e05bc32b13c2ac
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-13 06:41:41 -0800

    FThis file should contain the 5 functions arithmetic

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+/**
+ * op_add - addition
+ *
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: the sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - soustraction
+ *
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: the difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ *
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: the product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divition
+ *
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: the result of the division
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ *
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: the remainder of the division
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
