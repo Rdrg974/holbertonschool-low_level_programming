4a0086283d6bebd5d634b45214c7ffb3465947a2
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-13 01:32:16 -0800

    Define a new type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,16 +1,26 @@
 #ifndef __DOG__H__
 #define __DOG__H__
 
-struct dog
+/**
+ * dog_t - typedef for struct dog
+ *
+ * @name: first argument
+ * @age: second argument
+ * @owner: third argument
+ *
+ * Description: initialize a struct dog
+ */
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/**dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);*/
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
