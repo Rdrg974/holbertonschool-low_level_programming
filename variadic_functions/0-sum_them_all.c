d88120cf3ea3b521dcac50acf50861cd5a3bbae7
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-14 01:44:37 -0800

    Function that returns the sum of all its parameters

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -16,8 +16,6 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum;
 
-	if (n == 0)
-		return (0);
 	va_start(ap, n);
 	for (i = 0, sum = 0; i < n; i++)
 		sum += va_arg(ap, int);
