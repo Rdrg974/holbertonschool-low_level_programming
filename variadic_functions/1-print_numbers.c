2a5bf998b89dd497e2c2e3759ee47c62fe940b2b
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-14 02:12:41 -0800

    Function that prints numbers,

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * @separator: a const pointer to a char
+ * @n: a const integer always positive
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (separator != NULL)
+	{
+		va_start(ap, n);
+		for (i = 0; i < n ; i++)
+		{
+			if (i == (n - 1))
+				printf("%d\n", va_arg(ap, int));
+			else
+				printf("%d%s ", va_arg(ap, int), separator);
+		}
+		va_end(ap);
+	}
+}
