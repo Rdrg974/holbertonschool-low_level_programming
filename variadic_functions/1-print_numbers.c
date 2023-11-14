5b82d2624524d7a76e430bdc50726898c936e2fa
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-14 02:22:12 -0800

    Function that prints numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -25,7 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (i == (n - 1))
 				printf("%d", va_arg(ap, int));
 			else
-				printf("%d%s ", va_arg(ap, int), separator);
+				printf("%d%s", va_arg(ap, int), separator);
 		}
 		va_end(ap);
 	}
