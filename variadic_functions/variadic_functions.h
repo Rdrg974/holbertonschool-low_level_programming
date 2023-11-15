fbadb6222db8db88ba82978c4b0d97bbb4ea80cf
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-15 11:07:11 -0800

    The prototypes of all your functions

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,19 @@
 #ifndef __VARIADIC__FCT__H__
 #define __VARIABLE__FCT__H__
 
+#include <stdarg.h>
+
+/**
+ * struct print - print
+ * @type: The operator
+ * @f: The function associated
+ */
+typedef struct print
+{
+	char *type;
+	void (*f)(va_list ap);
+} print_type;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
