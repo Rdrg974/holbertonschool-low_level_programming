38beaa5c5423699c53b654be91c8d21f5146698a
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-27 03:21:53 -0800

    Creation of my function add_nod

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: a list
+ * @str: a string of character
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *new_str;
+	list_t *ptr = malloc(sizeof(list_t));
+
+	new_str = strdup(str);
+	ptr->str = new_str;
+	ptr->len = strlen(str);
+	ptr->next = *head;
+	*head = ptr;
+	return (*head);
+}
