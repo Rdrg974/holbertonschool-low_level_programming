ed9da20a24b71b19faeffdcbf0cb4997aa28615f
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-27 01:50:36 -0800

    Contains the prototypes of all my functions

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,30 @@
+#ifndef __LISTS__H__
+#define __LISTS__H__
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+
+#endif
