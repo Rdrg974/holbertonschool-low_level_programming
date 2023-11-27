e7db1203dc9290428401c892d74db5fcd4fe570c
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-27 07:28:04 -0800

    Modification of my function

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,15 +7,11 @@
 
 void free_list(list_t *head)
 {
-	list_t *current;
-	list_t *nxt;
-
-	current = head;
-	while (current != NULL)
+	if (head)
 	{
-		nxt = current->next;
-		free(current->str);
-		free(current);
-		current = nxt;
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
 	}
 }
