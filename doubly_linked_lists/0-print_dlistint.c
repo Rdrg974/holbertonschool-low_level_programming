17e9676e23734721f3aee231c8adaa4111f70fdb
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-29 03:47:57 -0800

    Modify the loop

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,16 +8,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
+	size_t count;
 
 	if (h == NULL)
 		return (-1);
-	for (count = 0; h->next != NULL; count++)
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	printf("%d\n", h->n);
-	count++;
 	return (count);
 }
