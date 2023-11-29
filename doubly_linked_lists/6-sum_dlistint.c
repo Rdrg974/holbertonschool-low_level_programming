0467a5f88def04318468f42fb88982cddf97e9cb
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-29 04:03:08 -0800

    Adding if the list is empty, return 0

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -10,11 +10,12 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
-	while (head->next != NULL)
+	if (head == NULL)
+		return (sum);
+	while (head != NULL)
 	{
 		sum += head->n;
 		head = head->next;
 	}
-	sum += head->n;
 	return (sum);
 }
