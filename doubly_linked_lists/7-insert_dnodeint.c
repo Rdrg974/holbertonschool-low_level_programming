c89732f32d958c3121da162ef1f74d70ec2cb7c8
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-29 06:39:43 -0800

    Correction of my first if

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -35,6 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (idx == 0)
 		{
+			newNode->next = *h;
 			if (*h)
 				(*h)->prev = newNode;
 			*h = newNode;
