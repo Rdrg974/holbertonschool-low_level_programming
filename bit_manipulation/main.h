9dc830c11492842fe5c131164557354b7b367385
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-30 05:36:43 -0800

    Adding the prototype pow_recursive

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define __MAIN__H__
 
 int _putchar(char c);
+unsigned long int _pow_recursion(int x, int y);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
