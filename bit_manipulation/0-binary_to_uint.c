e67463771fd875a79d392bb783e47a8b99fc47c5
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-30 13:53:20 -0800

    Adding if b is null and return 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int
@@ -11,6 +12,8 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int decimal;
 	unsigned int i;
 
+	if (b == NULL)
+		return (0);
 	for (decimal = 0, i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '1')
