095cba102ee47be32e1eb4391f4a86d34b98ae00
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-30 13:46:01 -0800

    Error mixed declarations and code

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,12 +9,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int mask;
 	int num_bits = sizeof(unsigned long int) * 8;
 
 	if (index >= num_bits)
 		return (-1);
-
-	unsigned long int mask = 1UL << index;
-
+	mask = 1UL << index;
 	return ((n & mask) ? 1 : 0);
 }
