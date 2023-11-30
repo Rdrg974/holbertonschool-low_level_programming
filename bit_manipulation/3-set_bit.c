e85c23e0776601d844db01f854751e02d4f22150
Rdrg974 <hassany.rodrigue@gmail.com>
2023-11-30 13:46:12 -0800

    Error mixed declarations and code

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -9,13 +9,12 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask;
 	int num_bits = sizeof(unsigned long int) * 8;
 
 	if (index >= num_bits)
 		return (-1);
-
-	unsigned long int mask = 1UL << index;
-
+	mask = 1UL << index;
 	*n |= mask;
 	return (1);
 }
