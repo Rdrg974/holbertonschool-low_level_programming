ebd60065f606edaf4b5593a869dd3c29b5e4b42f
Rdrg974 <hassany.rodrigue@gmail.com>
2023-12-04 01:45:16 -0800

    All the prototypes

---
+++ file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef __MAIN__H__
+#define __MAIN__H__
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
