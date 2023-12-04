5c52bbd3d7a635b35e7abbcfbc55af97066581cf
Rdrg974 <hassany.rodrigue@gmail.com>
2023-12-04 07:41:43 -0800

    Modify my code

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,38 +15,25 @@ int main(int ac, char **av)
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to"), exit(97);
 	fd1 = open(av[1], O_RDONLY);
-	if (access(av[2], F_OK) != -1)
-		fd2 = open(av[2], O_TRUNC | O_WRONLY);
-	else
-		fd2 = open(av[2], O_CREAT | O_WRONLY, mode);
+	if (fd1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	while ((bytes_read = read(fd1, buffer, sizeof(buffer))) > 0)
 	{
 		bytes_written = write(fd2, buffer, bytes_read);
 		if (bytes_written == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			close(fd1);
-			close(fd2);
-			exit(99);
-		}
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 	if (bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		close(fd1);
-		close(fd2);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
-	}
-	if (close(fd1) == -1 || close(fd2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd");
-		exit(100);
-	}
+	if (close(fd1) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
 	return (0);
 }
