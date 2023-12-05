3dcd4aa2efa36f4d2fdce47a1b11cc1b7a094300
Rdrg974 <hassany.rodrigue@gmail.com>
2023-12-05 02:00:53 -0800

    Remove the function to create an array of node

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,20 +1,6 @@
 #include "hash_tables.h"
 
 /**
- * array_of_node - create an array of node
- * Return: an array
- */
-hash_node_t *array_of_node()
-{
-	hash_node_t *array;
-
-	array = malloc(sizeof(hash_node_t));
-	if (array == NULL)
-		return (NULL);
-	return (array);
-}
-
-/**
  * hash_table_create - creates a hash table
  * @size: the size of array
  * Return: a pointer to the newly created hash table
@@ -37,16 +23,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 			free(str);
 			return (NULL);
 		}
-		for (i = 0; i < str->size; i++)
-		{
-			str->array[i] = array_of_node();
-			if (str->array[i] == NULL)
-			{
-				free(str->array);
-				free(str);
-				return (NULL);
-			}
-		}
+		for (i = 0; i < size; i++)
+			str->array[i] = NULL;
 		return (str);
 	}
 	return (NULL);
