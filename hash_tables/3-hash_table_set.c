831a02ec861550e5bbd051f108cdca7bba8cf009
Rdrg974 <hassany.rodrigue@gmail.com>
2023-12-06 14:25:50 -0800

    Adding that key can not be an empty string

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -33,7 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 
-	if (key == NULL || ht == NULL)
+	if (key[0] == '\0' || key == NULL || ht == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = create_node(key, value);
