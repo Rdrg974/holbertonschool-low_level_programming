684a208d987dbc1d4623549482e013e0c1da0fd2
Rdrg974 <hassany.rodrigue@gmail.com>
2023-12-06 14:16:19 -0800

    Shorten my code

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -31,36 +31,14 @@ hash_node_t *create_node(const char *key, const char *value)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node, *current;
+	hash_node_t *new_node;
 
-	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+	if (key == NULL || ht == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = create_node(key, value);
-	if (ht->array[index] == NULL)
-		ht->array[index] = new_node;
-	else
-	{
-		current = ht->array[index];
-		if (strcmp(current->key, key) == 0)
-		{
-			new_node->next = current->next;
-			ht->array[index] = new_node;
-			return (1);
-		}
-		while (current->next != NULL && strcmp(current->next->key, key) != 0)
-		{ current = current->next;
-		}
-		if (strcmp(current->key, key) == 0)
-		{
-			new_node->next = current->next->next;
-			current->next = new_node;
-		}
-		else
-		{
-			new_node->next = ht->array[index];
-			ht->array[index] = new_node;
-		}
-	}
+	if (ht->array[index])
+		new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
 }
