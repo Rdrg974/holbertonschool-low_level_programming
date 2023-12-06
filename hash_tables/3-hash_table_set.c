279135c7847a38ff4851a3a7ab24df4939635acb
Rdrg974 <hassany.rodrigue@gmail.com>
2023-12-06 14:50:30 -0800

    Update the key/value

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -31,16 +31,28 @@ hash_node_t *create_node(const char *key, const char *value)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *current;
 
-	if (key[0] == '\0' || key == NULL || ht == NULL)
+	if (key == NULL || ht == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = create_node(key, value);
 	if (new_node == NULL)
 		return (0);
+	current = ht->array[index];
 	if (ht->array[index])
-		new_node->next = ht->array[index];
+	{
+		while (current->next != NULL && strcmp(current->key, key) != 0)
+			current = current->next;
+		if (strcmp(current->key, key) == 0)
+			current->value = new_node->value;
+		else
+		{
+			new_node->next = ht->array[index];
+			ht->array[index] = new_node;
+		}
+		return (1);
+	}
 	ht->array[index] = new_node;
 	return (1);
 }
