a7542ba20dee78cc8873dc7f9f1c9d66ca19e32c
Rdrg974 <hassany.rodrigue@gmail.com>
2023-12-06 14:22:27 -0800

    Adding if the new_node is null, return 0

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -37,7 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = create_node(key, value);
-	if (new_node)
+	if (new_node == NULL)
 		return (0);
 	if (ht->array[index])
 		new_node->next = ht->array[index];
