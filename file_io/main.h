90885e291c14286fe4c93e0c6ea71d63a57327c0
Rdrg974 <hassany.rodrigue@gmail.com>
2023-12-07 06:45:50 -0800

    Remove BUFSIZE

--- file_io/main.h
+++ file_io/main.h
@@ -8,7 +8,6 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#define BUFSIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
