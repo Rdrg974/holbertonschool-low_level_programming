d1a97e9826be5ec59b611ea9c1671f5d3b38459c
Rdrg974 <hassany.rodrigue@gmail.com>
2023-12-07 06:37:59 -0800

    Adding differente 0

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,24 +12,34 @@ int main(int ac, char *av[])
 	int fd1, fd2;
 	int bytes_read, bytes_written;
 	char buffer[1024];
+	const char *file_from;
+	const char *file_to;
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to"), exit(97);
-	fd1 = open(av[1], O_RDONLY);
+	file_from = av[1];
+	file_to = av[2];
+	fd1 = open(file_from, O_RDONLY);
 	if (fd1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	fd2 = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, mode);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	fd2 = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, mode);
 	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	while ((bytes_read = read(fd1, buffer, BUFSIZE)) > 0)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
+	while ((bytes_read = read(fd1, buffer, BUFSIZE)) != 0)
 	{
 		bytes_written = write(fd2, buffer, bytes_read);
-		if (bytes_written == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		if (bytes_written != bytes_read)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
 	}
 	if (bytes_read == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
 	if (close(fd1) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
 	if (close(fd2) == -1)
