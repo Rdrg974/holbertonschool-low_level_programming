24e73d4da45232f69820dc714f2409bb07c1908d
Rdrg974 <hassany.rodrigue@gmail.com>
2023-12-08 11:19:45 -0800

    Adding the free

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,7 +15,18 @@ hash_node_t *create_node(const char *key, const char *value)
 	if (!new_node)
 		return (NULL);
 	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
 	new_node->next = NULL;
 	return (new_node);
 }
@@ -35,24 +46,37 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (key == NULL || ht == NULL)
 		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = create_node(key, value);
+
 	if (new_node == NULL)
 		return (0);
+
 	current = ht->array[index];
-	if (ht->array[index])
+
+	while (current != NULL)
 	{
-		while (current->next != NULL && strcmp(current->key, key) != 0)
-			current = current->next;
 		if (strcmp(current->key, key) == 0)
-			current->value = new_node->value;
-		else
 		{
-			new_node->next = ht->array[index];
-			ht->array[index] = new_node;
+			free(new_node->key);
+			free(new_node->value);
+			free(new_node);
+
+			free(current->value);
+			current->value = strdup(value);
+
+			if (current->value == NULL)
+			{
+				free(current->key);
+				free(current);
+				return (0);
+			}
+			return (1);
 		}
-		return (1);
+		current = current->next;
 	}
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
 }
