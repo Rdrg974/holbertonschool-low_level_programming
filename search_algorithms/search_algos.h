ab7fdfa1c242ce8bd03328efd69f0e2e146d8121
Rdrg974 <hassany.rodrigue@gmail.com>
2024-01-16 03:47:26 -0800

    All the protoypes of functions

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef __SEARCH__ALGOS__H__
+#define __SEARCH__ALGOS__H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
